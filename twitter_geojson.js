2f4414ae46acebc181bb2e323349ad4ab22959e5
Taylor Teske <tatornator12@gmail.com>
2017-03-11 11:07:49 -0500

    Create twitter_geojson.js

---
+++ twitter_geojson.js
@@ -0,0 +1,155 @@
+var tweets = {
+    "type": "FeatureCollection", 
+    "features": [
+        {
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [
+                    38.89817286, 
+                    -77.02741405
+                ]
+            }, 
+            "type": "Feature", 
+            "properties": {
+                "text": "On the way home from happy hour", 
+                "twitter_handle": "DeschainRo", 
+                "name": "Taylor Teske"
+            }
+        }, 
+        {
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [
+                    38.66561356, 
+                    -78.45790709
+                ]
+            }, 
+            "type": "Feature", 
+            "properties": {
+                "text": "Burritos and beer!", 
+                "twitter_handle": "DeschainRo", 
+                "name": "Taylor Teske"
+            }
+        }, 
+        {
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [
+                    38.56475952, 
+                    -78.3644638
+                ]
+            }, 
+            "type": "Feature", 
+            "properties": {
+                "text": "Last hike of the weekend!", 
+                "twitter_handle": "DeschainRo", 
+                "name": "Taylor Teske"
+            }
+        }, 
+        {
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [
+                    38.44959087, 
+                    -78.46559297
+                ]
+            }, 
+            "type": "Feature", 
+            "properties": {
+                "text": "On top of Bearfence Mountain!", 
+                "twitter_handle": "DeschainRo", 
+                "name": "Taylor Teske"
+            }
+        }, 
+        {
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [
+                    38.49939316, 
+                    -78.47204553
+                ]
+            }, 
+            "type": "Feature", 
+            "properties": {
+                "text": "Celebrating two year anniversary in Shenandoah.", 
+                "twitter_handle": "DeschainRo", 
+                "name": "Taylor Teske"
+            }
+        }, 
+        {
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [
+                    38.84881132, 
+                    -78.20516892
+                ]
+            }, 
+            "type": "Feature", 
+            "properties": {
+                "text": "Enjoying my day off in Shenandoah!", 
+                "twitter_handle": "DeschainRo", 
+                "name": "Taylor Teske"
+            }
+        }, 
+        {
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [
+                    39.06014687, 
+                    -77.15652886
+                ]
+            }, 
+            "type": "Feature", 
+            "properties": {
+                "text": "Founding Farmers for breakfast!", 
+                "twitter_handle": "DeschainRo", 
+                "name": "Taylor Teske"
+            }
+        }, 
+        {
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [
+                    39.0941721, 
+                    -77.20175057
+                ]
+            }, 
+            "type": "Feature", 
+            "properties": {
+                "text": "Last class of the week! Virginia tomorrow!", 
+                "twitter_handle": "DeschainRo", 
+                "name": "Taylor Teske"
+            }
+        }, 
+        {
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [
+                    38.8843039, 
+                    -77.02263191
+                ]
+            }, 
+            "type": "Feature", 
+            "properties": {
+                "text": "From 70 and sunny to 40 and windy....", 
+                "twitter_handle": "DeschainRo", 
+                "name": "Taylor Teske"
+            }
+        }, 
+        {
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [
+                    39.15021997, 
+                    -77.1803098
+                ]
+            }, 
+            "type": "Feature", 
+            "properties": {
+                "text": "My professor made me create a Twitter account for class!", 
+                "twitter_handle": "DeschainRo", 
+                "name": "Taylor Teske"
+            }
+        }
+    ]
+}
