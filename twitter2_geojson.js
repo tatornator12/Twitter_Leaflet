436476d8af31a1f685b2849978aa27e394fb1f8a
Taylor Teske <tatornator12@gmail.com>
2017-03-14 19:56:40 -0400

    Create twitter2_geojson.js

---
+++ twitter2_geojson.js
@@ -0,0 +1,21 @@
+tweets2 = {
+    "type": "FeatureCollection", 
+    "features": [
+        {
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [
+                    -77.24287239, 
+                    39.11278323
+                ]
+            }, 
+            "type": "Feature", 
+            "properties": {
+                "text": "Finally, some white in our winter lives! https://t.co/Fuj0MhU4VQ", 
+                "twitter_handle": "geotag3", 
+                "name": "geot",
+				"image": "https://pbs.twimg.com/media/C65zqF4WgAEELhf.jpg"
+            }
+        }
+    ]
+}
